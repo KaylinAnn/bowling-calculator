f5603781eef638350c9b4aa21ed041a08bd49c25
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-01-15 13:06:48 -0700

    all inputs are working and saving to state

--- src/App.js
+++ src/App.js
@@ -5,35 +5,69 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      scores: [
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null],
-        [null, null]
-      ]
+      "frame-1": { "ball-1": null, "ball-2": null },
+      "frame-2": { "ball-1": null, "ball-2": null },
+      "frame-3": { "ball-1": null, "ball-2": null },
+      "frame-4": { "ball-1": null, "ball-2": null },
+      "frame-5": { "ball-1": null, "ball-2": null },
+      "frame-6": { "ball-1": null, "ball-2": null },
+      "frame-7": { "ball-1": null, "ball-2": null },
+      "frame-8": { "ball-1": null, "ball-2": null },
+      "frame-9": { "ball-1": null, "ball-2": null },
+      "frame-10": { "ball-1": null, "ball-2": null }
     };
   }
+
+  calculateScore() {
+    console.log(this.state);
+  }
+
   render() {
+    const frames = [...Array(10).keys()].map(i => {
+      return (
+        <div className="frame">
+          <input
+            onChange={e => {
+              this.setState({
+                [`frame-${i + 1}`]: {
+                  "ball-1": e.target.value,
+                  "ball-2":
+                    e.target.value == 10
+                      ? null
+                      : this.state[`frame-${i + 1}`]["ball-2"]
+                }
+              });
+            }}
+            className="frame-input"
+            type="text"
+          />
+          <input
+            onChange={e => {
+              this.setState({
+                [`frame-${i + 1}`]: {
+                  "ball-1": this.state[`frame-${i + 1}`]["ball-1"],
+                  "ball-2": e.target.value
+                }
+              });
+            }}
+            className={`frame-input ${
+              this.state[`frame-${i + 1}`]["ball-1"] == 10
+                ? "input-disabled"
+                : ""
+            }`}
+            type="text"
+          />
+          <div>{this.state[`frame-${i + 1}`]["ball-1"]}</div>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
         <h1>Bowling Calculator</h1>
-        <div className="frame-container">
-          <div className="frame frame-one" />
-          <div className="frame frame-two" />
-          <div className="frame frame-three" />
-          <div className="frame frame-four" />
-          <div className="frame frame-five" />
-          <div className="frame frame-six" />
-          <div className="frame frame-seven" />
-          <div className="frame frame-eight" />
-          <div className="frame frame-nine" />
-          <div className="frame frame-ten" />
+        <div className="frame-container">{frames}</div>
+        <div>
+          <button onClick={() => this.calculateScore()}>Submit Score</button>
         </div>
       </div>
     );
